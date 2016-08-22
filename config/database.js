2dd3c97d805bccf4fe1d3252ada33790dd9ffb4e
Brian Wilson <email@brian-wilson.org>
2016-08-22 22:37:03 +0100

    fixed up DB connect string

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 module.exports = {
 
 	// the database url to connect
-	url : 'mongodb://node:nodeuser@mongo.onmodulus.net:27017/uwO3mypu'
+	url : 'mongodb://node-mongodb:2Ha2mqSbxfdACuQuoM6W51C4qVYY4FceI2MSkoGDfnqjtGez6u1LYOUW1uQJX2BfGToVYvGNGi6YAuabofwPMQ==@node-mongodb.documents.azure.com:10250/?ssl=true'
 }
