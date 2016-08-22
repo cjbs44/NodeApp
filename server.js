5e403a621d43a034c36cd3e1bd712bf1677a2f28
Brian Wilson <email@brian-wilson.org>
2016-08-22 22:11:16 +0100

    updated server.js for Azure Web Site

--- server.js
+++ server.js
@@ -2,5 +2,4 @@ var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello World this is a demo application using Node.JS\n');
-}).listen(1337, '127.0.0.1');
-console.log('Server running at http://127.0.0.1:1337/');
+}).listen(process.env.PORT || 8080);
